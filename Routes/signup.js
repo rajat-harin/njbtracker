e5e5ec378ed2673735216fe9dd202c0f443edd74
rajat-harin <rajat.harinkhede@gmail.com>
2020-07-20 19:03:47 +0530

    mysql to heroku pg

--- Routes/signup.js
+++ Routes/signup.js
@@ -1,56 +1,57 @@
-const express = require('express');
-const connection = require('../connection');
-const bcrypt = require('bcrypt');
+const express = require("express");
+const connection = require("../connection");
+const bcrypt = require("bcrypt");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-	res.render('signup', {});
+router.get("/", (req, res) => {
+  res.render("signup", {});
 
-	// connection.query('INSERT INTO Login (email,password,category,username,contact) VALUES ?',
-	// [email,password,category,username,contact],
-	// (err, result)=> {
-	// 	if(!err){
-	// 		res.send(req.body);
-	// 	}
-	// 	else
-	// 	{
-	// 		res.send('0');
-	// 	}
-	// });
+  // connection.query('INSERT INTO Login (email,password,category,username,contact) VALUES ?',
+  // [email,password,category,username,contact],
+  // (err, result)=> {
+  // 	if(!err){
+  // 		res.send(req.body);
+  // 	}
+  // 	else
+  // 	{
+  // 		res.send('0');
+  // 	}
+  // });
 });
 
-router.post('/', (req, res) => {
-	const {name, contact, email, username, password, category} = req.body;	
-	const saltRounds = 10;
+router.post("/", (req, res) => {
+  const { name, contact, email, username, password, category } = req.body;
 
-	bcrypt.genSalt(saltRounds, function (err, salt) {
-		if (!err) {
-			bcrypt.hash(password, salt, function (err, hash) {
-				if (!err) {
-					connection.query("INSERT INTO login (email,password,category,username,contact,name) VALUES  (?)",
-						[[email, hash, category, username, contact, name]],
-						(err, result) => {
-							if (!err) {
-								req.flash('success_msg','You are Registered Successfully');
-								res.redirect('/login');
-							}
-							else {
-								res.status(500).send(err);
-							}
-						});
-				}
-				else {
-					res.status(500).send(err);
-				}
-			});
-		}
-		else {
-			res.status(500).send(err);
-		}
-	});
-
+  connection.query(
+    "INSERT INTO login (email,password,category,username,contact,name) VALUES  ($1,$2,$3,$4,$5,$6)",
+    [email, password, category, username, contact, name],
+    (err, result) => {
+      if (!err) {
+        req.flash("success_msg", "You are Registered Successfully");
+        res.redirect("/login");
+      } else {
+        res.status(500).send(err);
+        console.log(err);
+      }
+    }
+  );
 });
 
+router.post("/app", (req, res) => {
+  const { name, contact, email, username, password, category } = req.body;
 
+  connection.query(
+    "INSERT INTO login (email,password,category,username,contact,name) VALUES  ($1,$2,$3,$4,$5,$6)",
+    [email, password, category, username, contact, name],
+    (err, result) => {
+      if (!err) {
+        res.send(1);
+      } else {
+        res.status(500).send(err);
+        console.log(err);
+      }
+    }
+  );
+});
 
 module.exports = router;
