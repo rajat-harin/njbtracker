d9f40498a5a04aab1272440016a72b0906d574b8
USER <vaishnavi.b.k.2000@gmail.com>
2020-07-29 19:10:55 +0530

    a

--- Routes/appCon.js
+++ Routes/appCon.js
@@ -98,14 +98,14 @@ router.get("/trip_info", (req, res) => {
 router.post("/order_info", (req, res) => {
     var final={};
     // res.send("abd");
-    connection.query("SELECT * FROM orders where sender_id = $1", [req.body.id], (err, result) => {
+    connection.query("SELECT * FROM orders where sender_id = $1", [req.body.id], (err, result1) => {
         if (!err) {
             // res.send(result.rows);
-            final.orders = result.rows;
-            connection.query("select * from products where id=$1", [result.rows.product_id], (err, result) => {
+            final.orders = result1.rows;
+            connection.query("select * from products where id=$1", [result1.rows.product_id], (err, result2) => {
                 if(!err)
                 {
-                    final.products=result.rows;
+                    final.products=result2.rows;
                 }
                 else {
                     final=-1;
