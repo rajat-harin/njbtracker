51739076210fc51f52857e4a48fd6f185bd5a0a8
USER <vaishnavi.b.k.2000@gmail.com>
2020-08-01 23:15:24 +0530

    22

--- Routes/appCon.js
+++ Routes/appCon.js
@@ -82,35 +82,35 @@ router.post("/trip_info", (req, res) => {
 
     let id = req.body.id;
     var final = {};
-    connection.query(
-        "select name from products ",
-        [id], (err, result) => {
-            if (!err) {
-                final.product = result.row;
-                res.send(result);
-                res.send(final);
-            } else {
-                res.send("-1");
-            }
-        }
-    );
+    // connection.query(
+    //     "select name from products",
+    //     [id], (err, result) => {
+    //         if (!err) {
+    //             final.product = result.row;
+    //             res.send(result);
+    //             res.send(final);
+    //         } else {
+    //             res.send("-1");
+    //         }
+    //     }
+    // );
     connection1.query("select * from places where id in (select source_id from orders where delivery_id in (select id from delivery_system where login_id=$1 ))", [id], (err1, result1) => {
         if (!err1) {
-            final.source = result1.row;
-            // res.send(final);
+            final.source = result1;
+            res.send(final);
         } else {
             res.send("-1");
         }
     });
 
-    connection2.query("select * from places where id in (select destination_id from orders where delivery_id in (select id from delivery_system where login_id=$1 ))", [id], (err2, result2) => {
-        if (!err2) {
-            final.destination = result2.row;
-        } else {
-            res.send("-1");
-        }
-    });
-    res.send(final);
+    // connection2.query("select * from places where id in (select destination_id from orders where delivery_id in (select id from delivery_system where login_id=$1 ))", [id], (err2, result2) => {
+    //     if (!err2) {
+    //         final.destination = result2.row;
+    //     } else {
+    //         res.send("-1");
+    //     }
+    // });
+    // res.send(final);
 
 });
 
