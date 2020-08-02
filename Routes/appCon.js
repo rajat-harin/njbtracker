22914abac24c5283ff0e1ce8a2cb816be372ebbf
USER <vaishnavi.b.k.2000@gmail.com>
2020-08-02 09:22:55 +0530

    again trying driver trip

--- Routes/appCon.js
+++ Routes/appCon.js
@@ -87,15 +87,16 @@ router.post("/trip_info", (req, res) => {
             // res.send(result.rows[0].source_id+" "+result.rows[0].destination_id+" "+result.rows[0].product_id+" "+id);
             if(!err)
             {
-                for (var i=0;i<result.length;i++)
-                {
-                    var s=result.rows[i].source_id;
-                    var d=result.rows[i].destination_id;
-                    res.send(s+" "+d);
-                    connection1.query("select * from places where id =$1 or id=$2",[s,d],(err1,result1)=>{
-                       res.send(result1.rows);
-                    });
-                }
+                res.send(result.length);
+                // for (var i=0;i<result.length;i++)
+                // {
+                //     var s=result.rows[i].source_id;
+                //     var d=result.rows[i].destination_id;
+                //     res.send(s+" "+d);
+                //     connection1.query("select * from places where id =$1 or id=$2",[s,d],(err1,result1)=>{
+                //        res.send(result1.rows);
+                //     });
+                // }
 
             }
             else
